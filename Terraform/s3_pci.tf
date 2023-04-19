2a25aaf884c25d9abcd346af806ca4a69e4f8b7c
Sam <8408317+samgabrail@users.noreply.github.com>
2023-04-19 16:36:13 +0000

    add kubernetes and custom policies for checkov

---
+++ Terraform/s3_pci.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "credit_cards_bucket" {
+  region        = var.region
+  bucket        = local.bucket_name
+  acl           = "public-read"
+  force_destroy = true
+
+  tags = {
+    Scope = "PCI",
+    
+  }
+}
