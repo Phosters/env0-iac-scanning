934ae364e9873a5ecc70915b42ab90b32b1a953c
Foster Obeng Gyamfi <94114815+Phosters@users.noreply.github.com>
2024-09-13 16:21:27 +0000

    Update run_checkov.sh

--- run_checkov.sh
+++ run_checkov.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
 # Run Checkov against the specified directory
-checkov -d . --quiet --output json > checkov_report.json
-
-# Exit with an error code if Checkov finds any issues
-if [ $? -ne 0 ]; then
-    echo "Checkov scan failed. Please review the report."
-    exit 1
-fi
-
+checkov -d ./Terraform
 
